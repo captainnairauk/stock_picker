d003bc9c1c20ffba85038fcd4a5d7de030673e0a
Aniket Kaimal <aniket1995@yahoo.co.in>
2022-02-28 23:23:19 +0530

    first commit

---
+++ stock_pickers.rb
@@ -0,0 +1,9 @@
+def stock_picker(array)
+pair_of_days=[]
+array.each_with_index do |element, i|
+    if array[i]<array[i-1] && array[i]<array[i+1]
+        pair_of_days.push(element)
+    elsif 
+    
+end
+end
